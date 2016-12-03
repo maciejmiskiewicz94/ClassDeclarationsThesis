1a4f1eb3e3ca9453deee3dfcc21591470ac1cef1
yulia-b <julie.bu@ya.ru>
2016-12-04 00:30:54 +0100

    Set homework view + minor changes

--- ClassDeclarationsThsesis/Controllers/HomeworkController.cs
+++ ClassDeclarationsThsesis/Controllers/HomeworkController.cs
@@ -24,34 +24,6 @@ namespace ClassDeclarationsThsesis.Controllers
         }
         
 
-        /*private DropDownList GetSubjects(string[] selectedValues)
-        {
-
-            List<Subject> Subjects = new List<Subject>()
-
-        {
-
-            new Subject() { subject_id = 1, name = "Numerical Methods", headteacher_id = 1, teachers = new List<User>() { new User() {name = "John", surname = "Doe", user_id = 2, user_type = 2, email = "john@mail.com", password = "dsds" }}, students = new List<User>() { new User() {name = "Jane", surname = "Doe", user_id = 3, user_type = 1, email = "jane@mail.com", password = "dsds" }}},
-
-            new Subject() { subject_id = 2, name = "Numerical Methods", headteacher_id = 1, teachers = new List<User>() { new User() {name = "John", surname = "Doe", user_id = 2, user_type = 2, email = "john@mail.com", password = "dsds" }}, students = new List<User>() { new User() {name = "Jane", surname = "Doe", user_id = 3, user_type = 1, email = "jane@mail.com", password = "dsds" }}},
-
-            
-        };
-            DropDownList s = new DropDownList();
-            s.Items.Insert(0,Subjects.ElementAt(0).name);
-            s.Items.Insert(1, Subjects.ElementAt(1).name);
-
-            return s;
-
-        }
-
-        public ActionResult MultiSelectSubject()
-        {
-
-            ViewBag.Subjectslist = GetSubjects(null);
-
-            return View();
-
-        }*/
+      
     }
 }
